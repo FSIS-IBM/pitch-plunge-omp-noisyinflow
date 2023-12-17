ca28a768f696b997d9f03afa134267f0e171d9dd
FSIS-IBM <154080697+FSIS-IBM@users.noreply.github.com>
2023-12-17 21:09:15 +0000

    Add files via upload

--- openmp.cmd
+++ openmp.cmd
@@ -2,7 +2,7 @@
 #PBS -o logfile.log
 #PBS -e errorfile.err
 #PBS -l walltime=168:00:00
-#PBS -l select=1:ncpus=40
+#PBS -l select=1:ncpus=16
 tpdir=`echo $PBS_JOBID | cut -f 1 -d .`
 tempdir=$HOME/scratch/job$tpdir
 mkdir -p $tempdir
@@ -10,6 +10,6 @@ cd $tempdir
 cp -R $PBS_O_WORKDIR/* .
 
 ulimit -s unlimited
-export OMP_NUM_THREADS=40
-./main.out > output.txt
+export OMP_NUM_THREADS=16
+./run_executable.out > output.txt
 mv ../job$tpdir $PBS_O_WORKDIR/. 
